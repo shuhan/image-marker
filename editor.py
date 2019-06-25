77a797f41a3a6f8c48f59b4814bd3267e633e039
Shah-nawaj Shuhan <s.shuhan@gmail.com>
2019-06-25 17:05:55 +0100

    Image marker complete

--- editor.py
+++ editor.py
@@ -1,9 +1,67 @@
 #!/usr/bin/env python3
-from flask import Flask, request, Markup, render_template, url_for
+import os
+import json
+from flask import Flask, request, Markup, render_template, url_for, jsonify
 
-app = Flask(__name__)
+app         = Flask(__name__)
+types       = { 'Obstacle' : False, 'MR.York' : False, 'Vehicle' : False, 'Wall' : False, 'RescueVehicle': False, 'Floor' : True }
+image_dir   = 'static/flight_capture'
+result_dir  = 'static/marked'
 
-@app.route("/")
+def get_first_image():
+
+    filelist = os.listdir(image_dir)
+    for fichier in filelist[:]: # filelist[:] makes a copy of filelist.
+        if not(fichier.endswith(".png")):
+            filelist.remove(fichier)
+    
+    if len(filelist) == 0:
+        return None
+
+    file_path = 'flight_capture/' + filelist[0]
+
+    return file_path
+
+
+@app.route("/", methods=['GET'])
 def index():
-    types = { 'Obstacle' : False, 'MR.York' : False, 'Vehicle' : False, 'Wall' : False, 'RescueVehicle': False, 'Floor' : True }
-    return render_template('index.html', image_url=url_for('static', filename='flight_capture/2019-06-21 19:16:12.png'), karnel_size=60, types=types)
+    file_path = get_first_image()
+
+    if file_path is None:
+        return render_template('marking_complete.html')
+
+    return render_template('index.html', image_url=url_for('static', filename=file_path), karnel_size=60, types=types)
+
+@app.route("/submit", methods=['GET', 'POST'])
+def submit():
+    content = request.json
+    if content is None or 'url' not in content.keys():
+        print("Invalid request")
+        return jsonify({ 'success' : False, 'message' : 'Invalid request' })
+
+    filename = os.path.basename(content['url'])
+    content['url'] = filename
+
+    if not os.path.isdir(result_dir):
+        os.makedirs(os.path.join(app.root_path, result_dir))
+
+    image_path  = os.path.join(app.root_path, image_dir, filename)
+    move_to     = os.path.join(app.root_path, result_dir, filename)
+    data_path   = os.path.join(app.root_path, result_dir, os.path.splitext(filename)[0] + '.json')
+
+    if not os.path.exists(image_path) or not os.path.isfile(image_path):
+        return jsonify({ 'success' : False, 'message' : 'File is already processed or invalid' })
+
+    os.rename(image_path, move_to)
+    datafile = open(data_path, "w")
+    datafile.write(json.dumps(content))
+    datafile.close()
+
+    file_path = get_first_image()
+
+    if file_path is None:
+        image_url = ''
+    else:
+        image_url = url_for('static', filename=file_path)
+
+    return jsonify({ 'success' : True, 'message' : 'Image stored', 'image_url' : image_url })
--- static/script.js
+++ static/script.js
@@ -18,6 +18,7 @@ $( document ).ready(function() {
         window.image_editor.start_top       = (window.image_editor.height - window.image_editor.actual_height)/2;
         window.image_editor.editor_width    = window.image_editor.actual_width + (window.image_editor.columns * 2 * window.image_editor.border_width);
         window.image_editor.editor_height   = window.image_editor.actual_height + (window.image_editor.rows * 2 * window.image_editor.border_width);
+        window.image_editor.marks           = {};
 
         //Update view
         $('.image-marker').css('width', window.image_editor.editor_width + 'px');
@@ -30,6 +31,7 @@ $( document ).ready(function() {
         {
             var x = (window.image_editor.start_left + (window.image_editor.karnel_size * c));
             var y = (window.image_editor.start_top + (window.image_editor.karnel_size * r));
+            var sequence = (r * window.image_editor.columns) + c;
             //Create the UI
             var fraction = document.createElement("div");
             fraction.id                         = 'fraction-' + c + '-' + r;
@@ -38,38 +40,141 @@ $( document ).ready(function() {
             fraction.style.backgroundImage      = "url('" + window.image_editor.url + "')";
             fraction.style.backgroundPositionX  = '-' + x + 'px';
             fraction.style.backgroundPositionY  = '-' + y + 'px';
-            fraction.className                  = 'fraction';
+            fraction.className                  = 'fraction sequence-' + sequence;
 
             fraction.setAttribute('column', c);
             fraction.setAttribute('row', r);
             fraction.setAttribute('x', x);
             fraction.setAttribute('y', y);
-            fraction.setAttribute('sequence', (r * window.image_editor.columns) + c);
+            fraction.setAttribute('sequence', sequence);
+
+            window.image_editor.marks[sequence] = [];
 
             $('.image-marker').append(fraction);
         }
 
+        /**
+         *  Handle click on each image tiles
+         */
         $('.fraction').on('click', function(){
             $(this).toggleClass('selected');
+
+            var sequence = $(this).attr('sequence');
+
+            if($(this).hasClass('selected')) {
+                if(window.image_editor.marks[sequence].indexOf(window.image_editor.current_item.name) === -1) {
+                    window.image_editor.marks[sequence].push(window.image_editor.current_item.name);
+                }
+            } else {
+                if(window.image_editor.marks[sequence].indexOf(window.image_editor.current_item.name) !== -1) {
+                    window.image_editor.marks[sequence].splice(window.image_editor.marks[sequence].indexOf(window.image_editor.current_item.name), 1 );
+                }
+            }
         });
 
-        window.image_editor.selection_types     = [];
-        window.image_editor.current_selection   = 0;
+        /**
+         * Handle click on next
+         */
+        $('.marker-button.next').on('click', function() {
+            window.image_editor.current_index++;
+            if(window.image_editor.current_index > window.image_editor.selection_types.length - 1)
+                window.image_editor.current_index = window.image_editor.selection_types.length - 1;
+            selectType()
+        });
+
+        /**
+         * Handle click on previous
+         */
+        $('.marker-button.previous').on('click', function() {
+            window.image_editor.current_index--;
+            if(window.image_editor.current_index < 0)
+                window.image_editor.current_index = 0;
+            selectType()
+        });
+
+        /**
+         * Handle click on submit
+         */
+        $('.marker-button.submit').on('click', function() {
+            //Set default value
+            $.each(window.image_editor.marks, function(key, value) {
+                if(value.length === 0)
+                    window.image_editor.marks[key].push(window.image_editor.default_type);
+            });
+
+            window.image_editor.response = {
+                'url'           : window.image_editor.url,
+                'karnel_size'   : window.image_editor.karnel_size,
+                'width'         : window.image_editor.width,
+                'height'        : window.image_editor.height,
+                'start_left'    : window.image_editor.start_left,
+                'start_top'     : window.image_editor.start_top,
+                'marks'         : window.image_editor.marks
+            };
+
+            $.ajax({
+                type: "POST",
+                url: '/submit',
+                data: JSON.stringify(window.image_editor.response),
+                contentType:"application/json; charset=utf-8",
+                dataType:"json",
+                success: function(data, textStatus, jqXHR) {
+                    if(data['success']) {
+                        image_url = data['image_url'];  //For now don't worry just refresh the page
+                        location.reload();
+                    } else {
+                        alert(data['message']);
+                        $('.marker-button.submit').prop("disabled", true);
+                    }
+                }
+            });
+        });
+
+        window.image_editor.selection_types = [];
+        window.image_editor.current_index   = 0;
 
         $.each(window.image_editor.types, function(key, value) {
             if(value) {
                 window.image_editor.default_type = key;
             } else {
-                window.image_editor.selection_types.push({'name' : key, 'default' : value, 'selection' : []});
+                window.image_editor.selection_types.push({'name' : key, 'default' : value});
             }
         });
 
-        selectType(window.image_editor.selection_types[window.image_editor.current_selection]);        
+        selectType();        
     });
 
-    function selectType(itemType) {
-        txt = "<p>Please select the square with: <b>" + itemType.name + "</b></p>";
+    function selectType() {
+        window.image_editor.current_item = window.image_editor.selection_types[window.image_editor.current_index];
+
+        txt = "<p>Please select the square with: <b>" + window.image_editor.current_item.name + "</b></p>";
         $('.image-marker-instructions').html(txt);
-        //Setup other configurations to keep track of the selections
+
+        $('.fraction').removeClass('selected');
+
+        $.each(window.image_editor.marks, function(key, value) {
+            if(value.indexOf(window.image_editor.current_item.name) !== -1) {
+                $('.fraction.sequence-' + key).addClass('selected');
+            }
+        });
+
+        if(window.image_editor.current_index === 0) {
+            if(!$('.marker-button.previous').hasClass('hidden'))
+                $('.marker-button.previous').addClass('hidden');
+        } else {
+            if($('.marker-button.previous').hasClass('hidden'))
+                $('.marker-button.previous').removeClass('hidden');
+        }
+        if(window.image_editor.current_index === window.image_editor.selection_types.length - 1) {
+            if(!$('.marker-button.next').hasClass('hidden'))
+                $('.marker-button.next').addClass('hidden');
+            if($('.marker-button.submit').hasClass('hidden'))
+                $('.marker-button.submit').removeClass('hidden');
+        } else {
+            if($('.marker-button.next').hasClass('hidden'))
+                $('.marker-button.next').removeClass('hidden');
+            if(!$('.marker-button.submit').hasClass('hidden'))
+                $('.marker-button.submit').addClass('hidden');
+        }
     }
 });

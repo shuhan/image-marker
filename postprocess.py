9be4d74dde35d4a5f6cccf772ccc72e1028aef54
Shah-nawaj Shuhan <ss2665@york.ac.uk>
2019-07-01 19:53:53 +0100

    Post Processor is Done

--- postprocess.py
+++ postprocess.py
@@ -4,6 +4,7 @@ import os
 import sys
 import json
 import cv2
+import random
 
 class PostProcess:
 
@@ -46,13 +47,48 @@ class PostProcess:
         '''
         process for each image
         '''
+        print("Processing {} ... ".format(image_file)),
         with open(config_file, 'r') as json_file:
             config = json.loads(json_file.read())
             
-            print(config['url'])
+            grid_size   = config['karnel_size']
+            x_offset    = config['start_left']
+            y_offset    = config['start_top']
+            width       = config['width']
+            height      = config['height']
+            cols        = (width - (2*x_offset))/grid_size
+            rows        = (height - (2*y_offset))/grid_size
 
             im = cv2.imread(image_file)
 
+            for r in range(0, rows):
+                for c in range(0, cols):
+                    seq     = (r * cols) + c
+                    index   = str(seq)
+                    if len(config['marks'][index]) > 1:
+                        continue
+                    cname = config['marks'][index][0]
+                    x = x_offset + (c * grid_size)
+                    y = y_offset + (r * grid_size)
+
+                    rolled_value = random.random()
+
+                    if rolled_value < self.training_ratio:
+                        set_name = 'training'
+                    elif rolled_value < self.training_ratio + self.validation_ratio:
+                        set_name = 'validation'
+                    else:
+                        set_name = 'test'
+
+                    file_dir    = os.path.join(self.out_dir, set_name, cname)
+                    file_path   = os.path.join(file_dir, os.path.splitext(config['url'])[0] + '-' + index + '.png')
+
+                    if not os.path.isdir(file_dir):
+                        os.makedirs(file_dir)
+
+                    cropped_img = im[y:y+grid_size, x:x+grid_size]
+                    cv2.imwrite(file_path, cropped_img)
+        print('Done')
 
 if __name__ == "__main__":
     PostProcess().process()

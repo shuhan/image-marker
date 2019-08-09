45176451bbf9455f75fcbcfd0e4749c55bb525a8
Shah-nawaj Shuhan <ss2665@york.ac.uk>
2019-08-09 18:34:08 +0100

    Vehicle detection

--- vehicle_detect.py
+++ vehicle_detect.py
@@ -3,7 +3,7 @@ import cv2
 import sys
 
 if len(sys.argv) != 2:
-    print("Usages: python bear_finder.py [input_image]")
+    print("Usages: python object_detect.py [input_image]")
     sys.exit()
 
 image_file = sys.argv[1]
@@ -32,20 +32,16 @@ for i in indices:
 
 threash = 90
 
-bear_mask   = cv2.inRange(hsvImage, np.array([16, 64, 60]), np.array([30, 150, 130])) & (expect_mask * 255)
-tshirt_mask = cv2.inRange(hsvImage, np.array([40, 40, 32]), np.array([75, 113, 66])) & (expect_mask * 255)
+vheilce_mask = cv2.inRange(hsvImage, np.array([0, 100, 85]), np.array([10, 255, 125])) & expect_mask
 
-kernel = np.ones((16,16),np.uint8)
+kernel = np.ones((8,8),np.uint8)
 
-_, bear_mask = cv2.threshold(cv2.blur(bear_mask, (5,5)), 127, 255, cv2.THRESH_BINARY)
+vheilce_region = cv2.dilate(vheilce_mask, kernel, iterations=2)
 
-_, tshirt_mask = cv2.threshold(cv2.blur(tshirt_mask, (5,5)), 127, 255, cv2.THRESH_BINARY)
-tshirt_region = cv2.dilate(tshirt_mask, kernel, iterations=2)
+vheilce_found = np.sum(vheilce_mask) > threash
 
-bear_found = np.sum(np.bitwise_and(bear_mask, tshirt_region)) > threash
-
-if bear_found:
-    im2, contours, hierarchy = cv2.findContours(tshirt_region, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+if vheilce_found:
+    im2, contours, hierarchy = cv2.findContours(vheilce_region, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     accepted_contures = []
     
     for contour in contours:
@@ -54,10 +50,12 @@ if bear_found:
         area = cv2.contourArea(contour)
         wh_ratio = float(w)/float(h)
         
-        if area > threash and area > 0.6 * w * h and wh_ratio > 0.7 and wh_ratio < 1.40 and np.sum(bear_mask[y:y+h, x:x+w])/255 > threash:
+        if area > threash and area > 0.6 * w * h and wh_ratio > 0.7 and wh_ratio < 1.40 and np.sum(expect_mask[y:y+h, x:x+w]) > (2*area)/3:
             accepted_contures.append(contour)
             cv2.rectangle(origImg, (x, y), (x + w, y + h), (255, 0, 0), 2)
     
-    cv2.imshow("Bear Found", origImg)
+cv2.imshow("Vehicle Found", origImg)
+cv2.imshow("Vehicle Mask", vheilce_mask)
+cv2.imshow("Vehicle Region", vheilce_region)
 
 cv2.waitKey(0)

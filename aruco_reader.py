5fac28e901854b13df63a669dee02670f9f66888
Shah-nawaj Shuhan <ss2665@york.ac.uk>
2019-08-13 20:12:53 +0100

    Working with aruco code

--- aruco_reader.py
+++ aruco_reader.py
@@ -9,15 +9,15 @@ if len(sys.argv) != 2:
     print("Usages: python cluster.py [input_image]")
     sys.exit()
 
-image_file = sys.argv[1]
+# image_file = sys.argv[1]
 
-frame = cv2.imread(image_file)
-gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+# frame = cv2.imread(image_file)
+# gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
+aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_50)
 parameters =  aruco.DetectorParameters_create()
-corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
-frame_markers = aruco.drawDetectedMarkers(frame.copy(), corners, ids)
+# corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
+# frame_markers = aruco.drawDetectedMarkers(frame.copy(), corners, ids)
 
 # plt.figure()
 # plt.imshow(frame_markers)
@@ -28,11 +28,11 @@ frame_markers = aruco.drawDetectedMarkers(frame.copy(), corners, ids)
 # plt.show()
 
 fig = plt.figure()
-nx = 4
-ny = 3
+nx = 2
+ny = 2
 for i in range(1, nx*ny+1):
     ax = fig.add_subplot(ny,nx, i)
-    img = aruco.drawMarker(aruco_dict,i, 700)
+    img = aruco.drawMarker(aruco_dict, i, 700)
     plt.imshow(img, cmap = mpl.cm.gray, interpolation = "nearest")
     ax.axis("off")
 
